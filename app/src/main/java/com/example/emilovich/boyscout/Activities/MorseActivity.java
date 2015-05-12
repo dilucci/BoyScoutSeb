bc63c454ab6bbe378e48eb23e14469c00d3ba9da
Fridthjof <efridthjof@gmail.com>
2015-05-12 18:30:03 +0200

    Alert i Morse. Samt funktionel Morse med fungerende threads.

--- app/src/main/java/com/example/emilovich/boyscout/Activities/MorseActivity.java
+++ app/src/main/java/com/example/emilovich/boyscout/Activities/MorseActivity.java
@@ -144,7 +144,7 @@ public class MorseActivity extends ActionBarActivity {
         alert.setMessage("Transmitting morse message: " + message);
         alert.setButton("Stop", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
-                morseHandler.stopThread();
+                morseHandler.lastwink();
                 finish();
             }
         });
--- app/src/main/java/com/example/emilovich/boyscout/Entities/MorseHandler.java
+++ app/src/main/java/com/example/emilovich/boyscout/Entities/MorseHandler.java
@@ -1,9 +1,6 @@
 package com.example.emilovich.boyscout.Entities;
 
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.hardware.Camera;
-import android.os.Handler;
 
 import java.util.ArrayList;
 
@@ -12,36 +9,40 @@ import java.util.ArrayList;
  */
 public class MorseHandler implements Runnable {
 
+
     private Camera camera;
     private Camera.Parameters params;
+    boolean stopblink;
     private ArrayList<String> sequence = new ArrayList<>();
 
-    public MorseHandler(ArrayList<String> morseSequence){
+    public MorseHandler(ArrayList<String> morseSequence) {
         if (camera == null || params == null) {
             setUpCamera();
+            stopblink = false;
         }
         this.sequence = morseSequence;
     }
 
     //check if phone has flashlight and sets up camera
-    private void setUpCamera(){
+    private void setUpCamera() {
         camera = Camera.open();
         params = camera.getParameters();
     }
 
-    private void flashlightOn(){
+    private void flashlightOn() {
         params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
         camera.setParameters(params);
         camera.startPreview();
     }
 
-    private void flashlightOff(){
+    private void flashlightOff() {
         params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
         camera.setParameters(params);
         camera.stopPreview();
     }
 
     public void stopThread() {
+
         if (camera != null) {
             camera.stopPreview();
             camera.release();
@@ -50,49 +51,58 @@ public class MorseHandler implements Runnable {
         Thread.currentThread().interrupt();
     }
 
+    public void lastwink (){
+        stopblink = true;
+    }
+
     @Override
     public void run() {
         String letterSeq = "";
         char letter;
-        for (int i = 0; i < sequence.size(); i++){
+
+        for (int i = 0; i < sequence.size(); i++) { //ORD
             letterSeq = sequence.get(i);
-            for (int k = 0; k < letterSeq.length(); k++){
-                letter = letterSeq.charAt(k);
-                if(letter == '.'){
+            for (int k = 0; k < letterSeq.length(); k++) { // bogstav
+                if (!stopblink) { //skulle køre sidste blink
+
+                    letter = letterSeq.charAt(k);
+                    if (letter == '.') {
+                        try {
+                            flashlightOn();
+                            Thread.sleep(250);
+                            flashlightOff();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (letter == '-') {
+                        try {
+                            flashlightOn();
+                            Thread.sleep(1000);
+                            flashlightOff();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (letter == '|') {
+                        try {
+                            Thread.sleep(2000); // Mellemrum!
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
                     try {
-                        flashlightOn();
-                        Thread.sleep(250);
-                        flashlightOff();
+                        Thread.sleep(400); // Efter hvert blink!
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-                if(letter == '-'){
-                    try {
-                        flashlightOn();
-                        Thread.sleep(1000);
-                        flashlightOff();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if(letter == '|'){
-                    try {
-                        Thread.sleep(2000); // Mellemrum!
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                    try {
-                    Thread.sleep(400); // Efter hvert blink!
+                else{stopThread();}
+                try {
+                    Thread.sleep(1000); // Efter hvert bogstav!
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            }
-            try {
-                Thread.sleep(1000); // Efter hvert bogstav!
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
         }
         stopThread();
