749164bc4fdf90b7b4eee01168baacc895b508f2
Michael <larsen_max@hotmail.com>
2015-05-19 13:52:41 +0200

    tilføjet camera intent til åbning af camera feature

--- app/src/main/java/com/example/emilovich/boyscout/Activities/CameraActivity.java
+++ app/src/main/java/com/example/emilovich/boyscout/Activities/CameraActivity.java
@@ -1,18 +1,75 @@
 package com.example.emilovich.boyscout.Activities;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.hardware.Camera;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.emilovich.boyscout.R;
 
 public class CameraActivity extends ActionBarActivity {
+    private boolean hasCamera;
+    private Context context;
+    private Camera camera;
+    private Camera.Parameters params;
+    private static final int IMAGE_CAPTURE = 102;
+    private Button buttonCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        initUI();
+    }
+
+    private void initUI() {
+        context = getApplicationContext();
+        hasCamera = context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+        buttonCamera = (Button) findViewById(R.id.buttonCamera);
+        if (hasCamera) {
+            buttonCamera.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, IMAGE_CAPTURE);
+                }
+            });
+        } else {
+            AlertDialog alert = new AlertDialog.Builder(CameraActivity.this).create();
+            alert.setTitle("No camera!");
+            alert.setMessage("Your device does not support this feature!");
+            alert.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alert.show();
+        }
+    }
+
+    public void setUpCamera() {
+        if (camera == null || params == null) {
+            camera = Camera.open();
+            params = camera.getParameters();
+        }
+    }
+
+    public void releaseCamera(){
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
     }
 
 
--- app/src/main/java/com/example/emilovich/boyscout/Activities/MorseActivity.java
+++ app/src/main/java/com/example/emilovich/boyscout/Activities/MorseActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
 import android.os.Vibrator;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
