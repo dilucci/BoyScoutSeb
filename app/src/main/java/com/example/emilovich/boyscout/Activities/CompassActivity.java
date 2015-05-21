11725335982d6365b8d279ba1d623133f37f9110
Michael <larsen_max@hotmail.com>
2015-05-21 14:25:33 +0200

    lavet compassactivity

--- app/src/main/java/com/example/emilovich/boyscout/Activities/CompassActivity.java
+++ app/src/main/java/com/example/emilovich/boyscout/Activities/CompassActivity.java
@@ -1,19 +1,38 @@
 package com.example.emilovich.boyscout.Activities;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 import com.example.emilovich.boyscout.R;
 
 
-public class CompassActivity extends ActionBarActivity {
+public class CompassActivity extends ActionBarActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private ImageView imageViewCompass;
+    private float currentDegree = 0f;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
+
+        initUI();
+    }
+
+    private void initUI() {
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        imageViewCompass = (ImageView)findViewById(R.id.imageViewCompass);
     }
 
 
@@ -37,5 +56,42 @@ public class CompassActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // get the angle around the z-axis rotated
+        float degree = Math.round(event.values[0]);
+
+        // create a rotation animation (reverse turn degree degrees)
+        RotateAnimation ra = new RotateAnimation(currentDegree, -degree, Animation.RELATIVE_TO_SELF, 0.5F, Animation.RELATIVE_TO_SELF, 0.5F);
+
+        // how long the animation will take place in millis
+        ra.setDuration(250);
+
+        // set the animation after the end of the reservation status
+        ra.setFillAfter(true);
+
+        //start the animation
+        imageViewCompass.startAnimation(ra);
+        currentDegree = -degree;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), sensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
